766365c31eed79234f6cf5f437918c9a627d3aee
Cole <65406787+ColeMaurer@users.noreply.github.com>
2021-06-11 18:39:30 -0500

    Debugging Changes
    Many changes. The script successfully places orders! Email and SMS functionality is still commented out.

--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -3,7 +3,7 @@
 import sqlite3
 import Config
 import datetime as dt
-from datetime import date, time
+from datetime import date, time, tzinfo
 import pandas as pd
 import threading
 import alpaca_trade_api as tradeapi
@@ -37,9 +37,18 @@ cursor.execute("""
 stocks = cursor.fetchall()
 symbols = [stock['symbol'] for stock in stocks]
 
+# SETTING UP TIME VARIABLES:
+# Set a constant for UTC timezone
+UTC = pytz.timezone('UTC')
 current_date = date.today().isoformat()
-start_minute_bar = f"{current_date} 09:30:00-04:00" # Market open       # Double check time zone
-end_minute_bar = f"{current_date} 09:45:00-04:00" # 15 minutes later
+start_minute_bar = f"{current_date}T09:30:00-04:00" # Market open       # Double check time zone
+end_minute_bar = f"{current_date}T09:45:00-04:00" # 15 minutes later
+
+# Get the current time, 15minutes, and 1 hour ago
+time_now = dt.datetime.now(tz=UTC)
+time_15_min_ago = time_now - dt.timedelta(minutes=15)
+time_1_hr_ago = time_now - dt.timedelta(hours=1)
+
 #api = tradeapi.REST(Config.API_KEY, Config.SECRET_KEY, base_url=Config.API_URL)
 api = tradeapi.REST(Config.API_KEY, Config.SECRET_KEY, Config.API_URL, 'v2') # added v2 to include updates.
 orders = api.list_orders(status='all', limit=500, after=f"{current_date}T13:30:00Z")
@@ -47,14 +56,6 @@ orders = api.list_orders(status='all', limit=500, after=f"{current_date}T13:30:0
 existing_order_symbols = [order.symbol for order in orders]
 
 messages = []
-
-# Set a constant for UTC timezone
-UTC = pytz.timezone('UTC')
-
-# Get the current time, 15minutes, and 1 hour ago
-time_now = dt.datetime.now(tz=UTC)
-time_15_min_ago = time_now - dt.timedelta(minutes=15)
-time_1_hr_ago = time_now - dt.timedelta(hours=1)
 
 for symbol in symbols:
     # Use the following once the alpaca account has been funded and polygon is accessible:
@@ -67,29 +68,50 @@ for symbol in symbols:
 # Get data from previous hour
 # If using the Free plan, the latest one can fetch is 15 minutes ago
     minute_bars = api.get_bars(symbol, TimeFrame.Minute,
-                                start=time_1_hr_ago.isoformat(),
+                                start=start_minute_bar,
                                 end=time_15_min_ago.isoformat(),
                                 adjustment='raw'
                                 ).df
-
+    #opening_range_mask = (minute_bars.index >= start_minute_bar) & (minute_bars.index < end_minute_bar)
+    #Need to change back to the line commented above when not just getting time from 1 hr ago.
     opening_range_mask = (minute_bars.index >= start_minute_bar) & (minute_bars.index < end_minute_bar)
     opening_range_bars = minute_bars.loc[opening_range_mask]
-    opening_range_low = opening_range_bars['low'].min()
-    opening_range_high = opening_range_bars['high'].max()
+    #print(opening_range_bars)
+    #opening_range_low = opening_range_bars['low'].min()
+    bars_df = pd.DataFrame(opening_range_bars, columns=['open', 'high', 'low', 'close', 'volume'])
+    bars_df_cols = bars_df.columns
+    #print(bars_df_cols)
+    #sorted_df = bars_df.sort_values(by='high', ascending=False)
+    #print('\nDataFrame datatypes :\n', bars_df.dtypes)
+    #bars_df.set_index(['open', 'high', 'low', 'close', 'volume'])
+    opening_range_low = bars_df['low'].min()
+    #opening_range_high = opening_range_bars['high'].max()
+    opening_range_high = bars_df['high'].max()
+    #print(opening_range_high)
+    #print("The above is the opening range high")
     opening_range = opening_range_high - opening_range_low
 
     after_opening_range_mask = minute_bars.index >= end_minute_bar
     after_opening_range_bars = minute_bars.loc[after_opening_range_mask]
-
-    after_opening_range_breakout = after_opening_range_bars[after_opening_range_bars['close'] > opening_range_high]
+    after_opening_range_df = pd.DataFrame(after_opening_range_bars, columns=['open', 'high', 'low', 'close', 'volume'])
+    #after_opening_range_breakout = after_opening_range_bars > opening_range_high
+    after_opening_range_breakout = after_opening_range_df[after_opening_range_df['high'] > opening_range_high]
+    after_opening_range_breakout_df = pd.DataFrame(after_opening_range_breakout, columns=['open', 'high', 'low', 'close', 'volume'])
+    #after_opening_range_breakout_df_close = pd.DataFrame(after_opening_range_breakout, columns=['close'])
+    #print(after_opening_range_breakout_df)
+    #print("-----------------")
+    #print(opening_range)
+    #print(after_opening_range_breakout_df_close)
     #print("Made it to line 85!")
     if not after_opening_range_breakout.empty: # if we have breakout candidates
         #print("We have breakout candidates?")
         if symbol not in existing_order_symbols: # if we have not already bought a stock
             #print("No orders yet")
-            limit_price = after_opening_range_breakout.iloc[0]['close']
-            messages.append(f"placing order for {symbol} at {limit_price}, closed above {opening_range_high}\n\n{after_opening_range_breakout.iloc[0]}\n\n")
-            print(f"placing order for {symbol} at {limit_price}, closed above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
+            #limit_price = after_opening_range_breakout.iloc[0]['close']
+            limit_price = after_opening_range_breakout_df['close'].min()
+            print(limit_price)
+            messages.append(f"placing order for {symbol} at {limit_price}, closed above {opening_range_high}\n\n")
+            print(f"placing order for {symbol} at {limit_price}, closed above {opening_range_high}")
 
             api.submit_order(
                 symbol=symbol,
@@ -108,7 +130,9 @@ for symbol in symbols:
             )
         else:
             print(f"Already an order for {symbol}, skipping")
-print(messages)
+#print(messages)
+#print(symbols)
+#print(minute_bars)
 #print("No breakout candidates determined")
 #with smtplib.SMTP_SSL(Config.EMAIL_HOST, Config.EMAIL_PORT, context=context) as server:
 #    server.login(Config.EMAIL_ADDRESS, Config.EMAIL_PASSWORD)
