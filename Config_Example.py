0541dc1724493f65643b5655941498e64f42e0a9
nickmaurer <colemaurer@gmail.com>
2021-07-13 18:59:29 -0500

    Config file example

---
+++ Config_Example.py
@@ -0,0 +1,15 @@
+# This is an example config file to be populated with the users account info
+
+# These keys can be found on the alpaca website
+API_KEY = ''
+SECRET_KEY = ''
+API_URL = 'https://paper-api.alpaca.markets'
+
+# Path to the app.db file
+DB_FILE = ''
+
+# Free gmail account info goes here if email notifs are wanted
+EMAIL_ADDRESS = ''
+EMAIL_PASSWORD = ''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
